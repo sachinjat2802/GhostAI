3a4aaa74a499070ca58c422a6df4bd8388d0e22a
sachinjat2802 <sachinjat2802@gmail.com>
2026-09-10 19:58:28 +0530

    fix: flatten repository structure to root level

--- Ghost AI/apps/desktop/src/core-engine/utils/EventBus.d.ts
+++
@@ -1,9 +0,0 @@
-import { EventEmitter } from 'events';
-/**
- * EventBus - Central pub/sub for all engine events
- * This decouples all services from each other
- */
-export declare class EventBus extends EventEmitter {
-    constructor();
-}
-//# sourceMappingURL=EventBus.d.ts.map
